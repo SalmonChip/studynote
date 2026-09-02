aeb3cc30e3efe7117e7088c4663c5c236aad03cd
“fish” <2678060651@qq.com>
2026-09-02 15:37:20 +0800

    首次提交启动类

---
+++ backend/src/main/java/com/studynote/notes/StudyNoteApplication.java
@@ -0,0 +1,28 @@
+package com.studynote.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @ClassName StudyNoteApplication
+ * @Description ToDo
+ * @Author Tong
+ * @LastChangeDate 2024-12-16 11:08
+ * @Version v1.0
+ */
+@SpringBootApplication
+@EnableScheduling
+public class StudyNoteApplication {
+    /*
+     * @Author:  Fish
+     * @date:  2026/9/2 15:03
+     * @Description: 测试一下git
+     * @params:
+     * @return:
+     */
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyNoteApplication.class, args);
+    }
+}
